f747342ff4b0dc9a7ce8280038fd46984c2ee829
Nikhil Kalige <nikhilkalige@gmail.com>
2015-09-24 11:27:15 -0700

    Initial package creation

---
+++ flask-nsq/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Nikhil Kalige'
+__email__ = 'nikhilkalige@gmail.com'
+__version__ = '0.1.0'
---
+++ tests/test_flask-nsq.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_flask-nsq
+----------------------------------
+
+Tests for `flask-nsq` module.
+"""
+
+import unittest
+
+from flask-nsq import flask-nsq
+
+
+class TestFlask-nsq(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
